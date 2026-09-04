e5571f5f733e6061a560bddbd84e1283db9edc87
Mark Angelo Tarvina <mttarvina@gmail.com>
2026-09-04 10:10:21 +0800

    Added support for basic plot streaming

--- signals_toolkit/src/signals_toolkit/plotter.py
+++ signals_toolkit/src/signals_toolkit/plotter.py
@@ -7,7 +7,10 @@ class SignalPlotter:
         self.plot = pg.mkQApp(f"{title}")
         self.canvas = pg.GraphicsLayoutWidget(show=True)
         self.canvas.resize(size[0], size[1])
-        self.plots: list = []
+        self.plot_areas: list = []
+        self.plot_waveforms: list = []
+        self.text_display: list = []
+        self.timer = None
 
     def add_plot(
         self,
@@ -26,19 +29,33 @@ class SignalPlotter:
         p.setLabel("left", y_label, unit=y_unit)
         p.showGrid(x=True, y=True, alpha=0.1)
         p.setRange(xRange=(np.min(x_data), np.max(x_data)), yRange=y_range, padding=0)
-        p.plot(x_data, y_data, pen=pg.mkPen(pen_color, width=1.0))
-        self.plots.append(p)
+        waveform = p.plot(x_data, y_data, pen=pg.mkPen(pen_color, width=1.0))
+        self.plot_areas.append(p)
+        self.plot_waveforms.append(waveform)
         self.canvas.nextRow()  # type: ignore
 
-    def update_plot(self, plot_index: int, x_data: np.ndarray, y_data: np.ndarray):
-        self.plots[plot_index].setData(x_data, y_data)
+    def update_plot(self, index: int, x_data: np.ndarray, y_data: np.ndarray):
+        self.plot_waveforms[index].setData(x_data, y_data)
 
     def add_text(self, text: str, plot_index: int, pos: tuple[int, int], color: str):
         text_item = pg.TextItem(text=text, color=color)
-        vb = self.plots[plot_index].getViewBox()
+        vb = self.plot_areas[plot_index].getViewBox()
         vb.addItem(text_item)
         text_item.setParentItem(vb)
         text_item.setPos(pos[0], pos[1])
+        self.text_display.append(text_item)
+
+    def update_text(self, index: int, text: str):
+        self.text_display[index].setText(text)
+
+    def setup_stream(self, plot_interval: float, callback_fn):
+        self.timer = pg.QtCore.QTimer()
+        self.timer.setInterval(int(plot_interval * 1000))
+        self.timer.timeout.connect(callback_fn)
+
+    def start_stream(self):
+        if self.timer is not None:
+            self.timer.start()
 
     def show(self):
         self.plot.exec()
